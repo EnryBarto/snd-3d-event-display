880b68eefca57864305a9594080ba02d2a1e5368
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2026-03-27 17:38:16 +0100

    feat: add ImGui hello world window

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,70 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+
+    // Init GLFW
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization error" << std::endl;
+        return -1;
+    } else {
+        std::cout << "GLFW initialization success" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+    glfwMakeContextCurrent(window);
+
+    // Init GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "GLAD initialization error" << std::endl;
+        return -1;
+    } else {
+        std::cout << "GLAD initialization success" << std::endl;
+    }
+
+    // Init ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.IniFilename = NULL; // Deactivate ini file
+    ImGui::StyleColorsDark(); // Set ImGUI dark theme
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
+    // Game Loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
+        // Start ImGui Frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Info");
+        ImGui::Text("Hello from ImGui!");
+        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+        ImGui::End();
+
+        // Rendering
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    std::cout << "Closing App" << std::endl;
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
