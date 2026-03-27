588aa6fd04f6e17decee649a5e0ee91ad200f38c
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2026-03-27 17:34:23 +0100

    build: add CMake dependencies
    - Integrate GLFW, GLM, and Assimp via FetchContent
    - Download ImGui and glad in external/
    - Optimize Assimp to build only the OBJ importer
    - Set GLFW to build statically for better portability

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
